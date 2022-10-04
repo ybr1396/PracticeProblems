ac97481ff8d8de58371e5f11998adb40c3d507b1
ybr1396 <60870859+ybr1396@users.noreply.github.com>
2022-10-04 19:37:31 +0530

    Add files via upload

---
+++ Exercise4.java
@@ -0,0 +1,31 @@
+package ybr.basics;
+
+import java.util.*;
+
+public class Exercise4 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Input value: ");
+		double input = in.nextDouble();
+		if (input > 0) {
+			if (input < 1) {
+				System.out.println("Positive small number ");
+			} else if (input > 100000) {
+				System.out.println("Positive large number ");
+			} else {
+				System.out.println("Positive number");
+			}
+		} else if (input < 0) {
+			if (Math.abs(input) < 1) {
+				System.out.println(" Negative small number ");
+			} else if (Math.abs(input) > 100000) {
+				System.out.println("Negative large number ");
+			} else {
+				System.out.println("Negative number");
+			}
+		} else {
+			System.out.println("Zero");
+		}
+		in.close();
+	}
+}
